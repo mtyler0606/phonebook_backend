a34dc975283bbc93de9530a0a50bfd35c84a036b
mtyler0606 <mtyler0606@gmail.com>
2025-01-08 14:00:47 -0600

    3.20 added validator for 'number', updated PUT method to validated updates

--- index.js
+++ index.js
@@ -143,7 +143,10 @@ app.put('/api/persons/:id', (request, response, next) => {
         number: body.number
     }
 
-    Person.findByIdAndUpdate(request.params.id, person, {new: true})
+    Person.findByIdAndUpdate(
+        request.params.id,
+        person,
+        {new: true, runValidators:true, context: 'query'})
         .then(updatedPerson => {
             response.json(updatedPerson)
         })
--- models/people.js
+++ models/people.js
@@ -19,8 +19,16 @@ const personSchema = new mongoose.Schema({
         minLength: 3,
         required: true
     },
-    number: String
-})
+    number: {
+        type: String,
+        validate: {
+            validator: function(v){
+                return /^[0-9]{2,3}?-\d{1,}$/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number`
+        },
+        required: [true, 'Phone number required']
+}})
 
 personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
