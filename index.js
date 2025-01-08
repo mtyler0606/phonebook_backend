3a378ea3e7efcf723d5c3e9f3c7728f9ff01e5a0
mtyler0606 <mtyler0606@gmail.com>
2025-01-08 13:30:08 -0600

    3.19 added validation when adding new entries

--- index.js
+++ index.js
@@ -38,6 +38,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === "CastError"){
         return response.status(400).send({ error: 'malformatted id'})
+    } else if (error.name === "ValidationError"){
+        return response.status(400).json({error: error.message})
     }
 
     next(error)
@@ -103,7 +105,7 @@ app.delete('/api/persons/:id', (request, response) => {
 })
 
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
     const name = body.name
     const number = body.number
@@ -129,6 +131,7 @@ app.post('/api/persons', (request, response) => {
     person.save().then(savedPerson => {
         response.json(savedPerson)
     })
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
